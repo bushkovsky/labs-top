2ebfc6264638c310b644eb3c3ecdeda394895f23
bushkovsky <62966446+bushkovsky@users.noreply.github.com>
2023-12-23 13:38:32 +0300

    features lab-5

---
+++ src/Lab5/ApplicationModels/DomainModels/IAccount.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.DomainModels;
+
+public interface IAccount
+{
+    public int CheckAccountBalance();
+    public void DecreaseAccountBalance(int number);
+    public void IncreaseAccountBalance(int number);
+    public IList<string> CheckHistory();
+}
---
+++ src/Lab5/CLI/Commands/CheckAccountHistory.cs
@@ -0,0 +1,25 @@
+﻿using Application.Ports;
+
+namespace CLI.Commands;
+
+public class CheckAccountHistory : ICommand
+{
+    public CheckAccountHistory(IUserPort port)
+    {
+        Port = port;
+    }
+
+    public IUserPort Port { get; private set; }
+
+    public string? CommandExecute()
+    {
+        string result = string.Empty;
+        IList<string> history = Port.CheckHistory();
+        foreach (string elementOfHistory in history)
+        {
+            result += elementOfHistory + '\n';
+        }
+
+        return result;
+    }
+}
